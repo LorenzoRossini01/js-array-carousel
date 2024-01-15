47520a3b969eb0516262f2c879a0116883881533
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-15 18:27:18 +0100

    feat: add image preview in js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 let slideIndex = 0;
 let slideHtml = "";
 
+let slidePreviewHtml = "";
+const thumbnailsContainerEl = document.getElementById("thumbnails-container");
+
 for (let i = 0; i < slides.length; i++) {
   const currentSlide = slides[i];
   let activeClass = "";
@@ -19,14 +22,24 @@ for (let i = 0; i < slides.length; i++) {
 
   slideHtml += `<img src="./img/${currentSlide}"
     class="slide ${activeClass}" />`;
+
+  slidePreviewHtml += `
+  <div class="slide-preview">
+    <img src="./img/${currentSlide}" class="slide-small ${activeClass}"  />
+  </div>`;
 }
 slideContainerEl.innerHTML = slideHtml;
+thumbnailsContainerEl.innerHTML = slidePreviewHtml;
 
 prevSlideArrowEl.addEventListener("click", function () {
   const allSlide = document.getElementsByClassName("slide");
+  const allSlidePreview = document.getElementsByClassName("slide-small");
 
   const oldSlide = allSlide[slideIndex];
   oldSlide.classList.remove("active");
+
+  const oldPreview = allSlidePreview[slideIndex];
+  oldPreview.classList.remove("active");
 
   if (slideIndex <= 0) {
     slideIndex = allSlide.length - 1;
@@ -36,13 +49,19 @@ prevSlideArrowEl.addEventListener("click", function () {
 
   const newSlide = allSlide[slideIndex];
   newSlide.classList.add("active");
+
+  const NewPreview = allSlidePreview[slideIndex];
+  NewPreview.classList.add("active");
 });
 
 nextSlideArrowEl.addEventListener("click", function () {
   const allSlide = document.getElementsByClassName("slide");
+  const allSlidePreview = document.getElementsByClassName("slide-small");
 
   const oldSlide = allSlide[slideIndex];
   oldSlide.classList.remove("active");
+  const oldPreview = allSlidePreview[slideIndex];
+  oldPreview.classList.remove("active");
 
   if (slideIndex >= allSlide.length - 1) {
     slideIndex = 0;
@@ -52,4 +71,7 @@ nextSlideArrowEl.addEventListener("click", function () {
 
   const newSlide = allSlide[slideIndex];
   newSlide.classList.add("active");
+
+  const NewPreview = allSlidePreview[slideIndex];
+  NewPreview.classList.add("active");
 });
