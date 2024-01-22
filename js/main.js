67f80cf6f6fcdd2e58c3eeb4ed415d3d9c108610
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-22 17:00:35 +0100

    fix: thumbnails click and use function to change images

--- js/main.js
+++ js/main.js
@@ -1,77 +1,77 @@
+const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
+let activeImage = 0;
+
 const slideContainerEl = document.getElementById("slide-container");
+const thumbnailsContainerEl = document.getElementById("thumbnails-container");
 const prevSlideArrowEl = document.getElementById("prev-arrow");
 const nextSlideArrowEl = document.getElementById("next-arrow");
 
-const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
-
-let slideIndex = 0;
+// genero le slide
 let slideHtml = "";
-
-let slidePreviewHtml = "";
-const thumbnailsContainerEl = document.getElementById("thumbnails-container");
-
 for (let i = 0; i < slides.length; i++) {
   const currentSlide = slides[i];
-  let activeClass = "";
-
-  if (i == slideIndex) {
-    activeClass = "active";
-  } else {
-    activeClass = "";
-  }
-
+  let activeClass = i === activeImage ? "active" : "";
   slideHtml += `<img src="./img/${currentSlide}"
-    class="slide ${activeClass}" />`;
-
-  slidePreviewHtml += `
-  <div class="slide-preview">
-    <img src="./img/${currentSlide}" class="slide-small ${activeClass}"  />
-  </div>`;
+class="slide ${activeClass}" />`;
 }
 slideContainerEl.innerHTML = slideHtml;
+
+// genero le thumbnails
+let slidePreviewHtml = "";
+for (let i = 0; i < slides.length; i++) {
+  const currentSlide = slides[i];
+  let activeClass = i === activeImage ? "active" : "";
+
+  slidePreviewHtml += `
+  <div class="slide-preview" >
+    <img src="./img/${currentSlide}" class="slide-small ${activeClass}"/>
+  </div>`;
+}
 thumbnailsContainerEl.innerHTML = slidePreviewHtml;
 
+// recupero le thumbnails
+const allThumbs = document.getElementsByClassName("slide-small");
+
+// click on prev button
 prevSlideArrowEl.addEventListener("click", function () {
-  const allSlide = document.getElementsByClassName("slide");
-  const allSlidePreview = document.getElementsByClassName("slide-small");
-
-  const oldSlide = allSlide[slideIndex];
-  oldSlide.classList.remove("active");
-
-  const oldPreview = allSlidePreview[slideIndex];
-  oldPreview.classList.remove("active");
-
-  if (slideIndex <= 0) {
-    slideIndex = allSlide.length - 1;
-  } else {
-    slideIndex--;
-  }
-
-  const newSlide = allSlide[slideIndex];
-  newSlide.classList.add("active");
-
-  const NewPreview = allSlidePreview[slideIndex];
-  NewPreview.classList.add("active");
+  const prevSlideIndex = activeImage - 1;
+  goToSlide(prevSlideIndex);
 });
 
+// click on next button
 nextSlideArrowEl.addEventListener("click", function () {
-  const allSlide = document.getElementsByClassName("slide");
-  const allSlidePreview = document.getElementsByClassName("slide-small");
+  const nextSlideIndex = activeImage + 1;
+  goToSlide(nextSlideIndex);
+});
 
-  const oldSlide = allSlide[slideIndex];
-  oldSlide.classList.remove("active");
-  const oldPreview = allSlidePreview[slideIndex];
-  oldPreview.classList.remove("active");
+// click on thumb
+for (let i = 0; i < allThumbs.length; i++) {
+  const thumb = allThumbs[i];
+  thumb.setAttribute("data-active-image", i);
 
-  if (slideIndex >= allSlide.length - 1) {
-    slideIndex = 0;
-  } else {
-    slideIndex++;
+  thumb.addEventListener("click", function () {
+    thumbIndex = parseInt(thumb.getAttribute("data-active-image"));
+    goToSlide(thumbIndex);
+  });
+}
+
+function goToSlide(slideIndex) {
+  const allThumbs = document.getElementsByClassName("slide-small");
+  const oldThumb = allThumbs[activeImage];
+  oldThumb.classList.remove("active");
+  activeClass = "active";
+  activeImage = slideIndex;
+  if (activeImage >= allThumbs.length) {
+    activeImage = 0;
+  } else if (activeImage < 0) {
+    activeImage = allThumbs.length - 1;
   }
 
-  const newSlide = allSlide[slideIndex];
-  newSlide.classList.add("active");
+  const newThumb = allThumbs[activeImage];
+  newThumb.classList.add("active");
 
-  const NewPreview = allSlidePreview[slideIndex];
-  NewPreview.classList.add("active");
-});
+  const currentSlide = slides[activeImage];
+  slideHtml = `<img src="./img/${currentSlide}"
+class="slide active" />`;
+  slideContainerEl.innerHTML = slideHtml;
+}
